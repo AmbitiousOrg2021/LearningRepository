c17dbc1b117daf038d43cf8afe18e7e66227b0a6
Sarthak Tyagi <59991590+Sweet-Blood@users.noreply.github.com>
2021-04-14 12:29:24 +0530

    Update AssignQues6.js

--- Assignment-1.js/AssignQues6.js
+++ Assignment-1.js/AssignQues6.js
@@ -1,5 +1,6 @@
 // W.A.P to reverse a number.
-
+// This program written below does not work for javascript so you can move forward to other questions
+// will upload the solution soon and with a reason.
 a = 9735;
 while(a != 0){
     num = (a%10);
